0716df32aaebfa04cde1700c4461e0976fddbbb6
The-Voidwalker <voidwalker.wikipedia@gmail.com>
2025-02-16 02:31:03 -0500

    finish basic implementation of OpenSSLHttpHandler
    still need to improve handling of request and response, but this is technically viable at this point

--- OpenSSLWebClient/Client/OpenSSLHttpHandler.cs
+++ OpenSSLWebClient/Client/OpenSSLHttpHandler.cs
@@ -1,14 +1,18 @@
-﻿using System;
+﻿using OpenSSLWebClient.Components;
+using OpenSSLWebClient.Exceptions;
+using System;
+using System.Linq;
 using System.Net.Http;
+using System.Text;
 using System.Threading;
 using System.Threading.Tasks;
 
 namespace OpenSSLWebClient.Client
 {
-    class OpenSSLHttpHandler : HttpMessageHandler
+    public class OpenSSLHttpHandler : HttpMessageHandler
     {
         private bool _disposed = false;
-        
+
         protected override Task<HttpResponseMessage> SendAsync(HttpRequestMessage request, CancellationToken cancellationToken)
         {
             if (request == null)
@@ -27,7 +31,61 @@ namespace OpenSSLWebClient.Client
                 return Task.FromException<HttpResponseMessage>(error);
             }
 
-            return null;
+            HttpResponseMessage response = new HttpResponseMessage();
+
+            // TODO: SSL object should be managed in a way that lets us reuse it safely
+            using (SSL ssl = new SSL(request.RequestUri.Host, "443"))
+            {
+                string message = request.Method.Method + " " + request.RequestUri.PathAndQuery
+                    + " HTTP/" + request.Version.Major + "." + request.Version.Minor + "\r\n";
+                foreach (var rheader in request.Headers)
+                {
+                    message += rheader.Key + ":" + string.Join(",", rheader.Value.ToArray()) + "\r\n";
+                }
+
+                if (request.Content != null)
+                {
+                    foreach (var cheader in request.Content.Headers)
+                    {
+                        message += cheader.Key + ":" + string.Join(",", cheader.Value.ToArray()) + "\r\n";
+                    }
+
+                    string content = request.Content.ReadAsStringAsync().Result;
+                    message += content;  // TODO: ensure message ends with \r\n\r\n in this case
+                }
+
+                message += "\r\n";
+
+                ssl.Connect();
+                ssl.Write(message);
+
+                byte[] buf = new byte[4098];
+                string resp = "";
+                int read = 0;
+
+                try
+                {
+                    while ((read = ssl.Read(ref buf)) > 0)
+                    {
+                        resp += Encoding.ASCII.GetString(buf, 0, read);
+                        // Represents end of response
+                        if (resp.Substring(resp.Length - 4) == "\r\n\r\n")
+                        {
+                            break;
+                        }
+                    }
+                }
+                catch (InteropException)
+                {
+                    // Some failures can be non-fatal. In future, we check those. For now, we cry and move on.
+                    throw;
+                }
+
+                ByteArrayContent responseContent = new ByteArrayContent(Encoding.UTF8.GetBytes(resp));
+                response.Content = responseContent;
+            }
+
+            return Task.FromResult(response);
         }
 
         /// <summary>
@@ -47,6 +105,11 @@ namespace OpenSSLWebClient.Client
             if (requestUri.Scheme != "https")
             {
                 return new InvalidOperationException("OpenSSLWebClient only supports the HTTPS scheme.");
+            }
+
+            if (request.Headers.Host == null)
+            {
+                request.Headers.Host = requestUri.Host;
             }
 
             return null;
