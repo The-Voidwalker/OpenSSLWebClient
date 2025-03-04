e6d5f0124264950afe6159f58713a77e2dab0fe0
The-Voidwalker <voidwalker.wikipedia@gmail.com>
2025-03-04 00:17:39 -0500

    Split SSLInterop and BIOInterop to separate files

--- OpenSSLWebClient/Components/BIO.cs
+++ OpenSSLWebClient/Components/BIO.cs
@@ -5,149 +5,6 @@ using System.Runtime.InteropServices;
 namespace OpenSSLWebClient.Components
 {
     /// <summary>
-    /// Contains all P/Invoke declarations for manipulating BIO objects.
-    /// </summary>
-    internal class BIOInterop
-    {
-        /// <summary>
-        /// Performs a lookup to convert a hostname and port into a valid <c>BioAddrInfo</c> object to connect to later.
-        /// <see href="https://docs.openssl.org/3.4/man3/BIO_ADDRINFO/"/>
-        /// <example> Recommended use:
-        /// <code>
-        /// IntPtr resPtr = Marshal.AllocHGlobal(Marshal.SizeOf(typeof(IntPtr)));
-        /// BIO_lookup_ex(..., resPtr);
-        /// IntPtr res = Marshal.ReadIntPtr(resPtr);
-        /// // res is now valid for BIO_ADDRINFO_* calls
-        /// </code>
-        /// </example>
-        /// </summary>
-        /// <param name="hostname">Hostname of remote service</param>
-        /// <param name="port">Port number for remote service</param>
-        /// <param name="lookup_type">Use <c>Constants.BIO_LOOKUP_CLIENT</c></param>
-        /// <param name="family">Address family, typically <c>AF_INET</c> or <c>AF_INET6</c></param>
-        /// <param name="socktype">Expected socket type, either <c>SOCK_STREAM</c> or <c>SOCK_DGRAM</c></param>
-        /// <param name="protocol">Specifies protocol, setting 0 will accept any</param>
-        /// <param name="res">Pointer to a pointer to the start of a <c>BIO_ADDRINFO</c> chain</param>
-        /// <returns><c>true</c> if operation completed successfully</returns>
-        [DllImport("libcrypto-3.dll", CallingConvention = CallingConvention.Cdecl)]
-        internal static extern bool BIO_lookup_ex(string hostname, string port, int lookup_type, int family, int socktype, int protocol, IntPtr res);
-
-        /// <summary>
-        /// Fetches the pointer of the next <c>BIO_ADDRINFO</c> object in the chain.
-        /// <see href="https://docs.openssl.org/3.4/man3/BIO_ADDRINFO/"/>
-        /// </summary>
-        /// <param name="bai">Pointer to a <c>BIO_ADDRINFO</c> object in unmanaged memory</param>
-        /// <returns>
-        /// IntPtr.Zero if there is no next <c>BIO_ADDRINFO</c>
-        /// </returns>
-        /// <remarks>Return case of IntPtr.Zero is untested!</remarks>
-        [DllImport("libcrypto-3.dll", CallingConvention = CallingConvention.Cdecl)]
-        internal static extern IntPtr BIO_ADDRINFO_next(IntPtr bai);
-
-        /// <summary>
-        /// Returns the address family of the given <c>BIO_ADDRINFO</c>.
-        /// <see href="https://docs.openssl.org/3.4/man3/BIO_ADDRINFO/"/>
-        /// </summary>
-        /// <param name="bai">Pointer to <c>BIO_ADDRINFO</c> object</param>
-        /// <returns>One of <c>AF_INET</c>, <c>AF_INET6</c>, and <c>AF_UNIX</c></returns>
-        [DllImport("libcrypto-3.dll", CallingConvention = CallingConvention.Cdecl)]
-        internal static extern int BIO_ADDRINFO_family(IntPtr bai);
-
-        /// <summary>
-        /// Gets the <c>BIO_ADDR</c> information from a <c>BIO_ADDRINFO</c> object.
-        /// <see href="https://docs.openssl.org/3.4/man3/BIO_ADDRINFO/"/>
-        /// </summary>
-        /// <param name="bai">Pointer to a <c>BIO_ADDRINFO</c> object</param>
-        /// <returns>Pointer to a <c>BIO_ADDR</c> object in unmanaged memory</returns>
-        [DllImport("libcrypto-3.dll", CallingConvention = CallingConvention.Cdecl)]
-        internal static extern IntPtr BIO_ADDRINFO_address(IntPtr bai);
-
-        /// <summary>
-        /// Frees memory used by a chain of <c>BIO_ADDRINFO</c> objects.
-        /// <see href="https://docs.openssl.org/3.4/man3/BIO_ADDRINFO/"/>
-        /// </summary>
-        /// <param name="bai">Pointer to first <c>BIO_ADDRINFO</c> object in a chain</param>
-        [DllImport("libcrypto-3.dll", CallingConvention = CallingConvention.Cdecl)]
-        internal static extern void BIO_ADDRINFO_free(IntPtr bai);
-
-        /// <summary>
-        /// Cross platform method for creating a socket.
-        /// <see href="https://docs.openssl.org/3.4/man3/BIO_connect/"/>
-        /// </summary>
-        /// <param name="domain">Best fetched using <c>BIO_ADDRINFO_family</c></param>
-        /// <param name="socktype">One of <c>SOCK_STREAM</c> or <c>SOCK_DGRAM</c></param>
-        /// <param name="protocol">Setting to 0 accepts any protocol</param>
-        /// <param name="options">Unused, set 0</param>
-        /// <returns>
-        /// Socket number if successful.
-        /// -1 on error.
-        /// </returns>
-        [DllImport("libcrypto-3.dll", CallingConvention = CallingConvention.Cdecl)]
-        internal static extern int BIO_socket(int domain, int socktype, int protocol, int options);
-
-        /// <summary>
-        /// Connects socket to specified address.
-        /// <see href="https://docs.openssl.org/3.4/man3/BIO_connect/"/>
-        /// </summary>
-        /// <param name="sock">Socket number</param>
-        /// <param name="addr">Pointer to address information, fetch using <c>BIO_ADDRINFO_address</c></param>
-        /// <param name="options">May be zero or any combination of <c>Constants.BIO_SOCK_*</c> constants</param>
-        /// <returns>1 on success, 0 on error</returns>
-        [DllImport("libcrypto-3.dll", CallingConvention = CallingConvention.Cdecl)]
-        internal static extern bool BIO_connect(int sock, IntPtr addr, int options);
-
-        /// <summary>
-        /// Closes specified socket.
-        /// <see href="https://docs.openssl.org/3.4/man3/BIO_connect/"/>
-        /// </summary>
-        /// <param name="sock">Socket number</param>
-        /// <returns>1 on success, 0 on error, should be safe to discard</returns>
-        [DllImport("libcrypto-3.dll", CallingConvention = CallingConvention.Cdecl)]
-        internal static extern bool BIO_closesocket(int sock);
-
-        /// <summary>
-        /// Returns socket BIO method.
-        /// <see href="https://docs.openssl.org/3.4/man3/BIO_s_socket/"/>
-        /// </summary>
-        [DllImport("libcrypto-3.dll", CallingConvention = CallingConvention.Cdecl)]
-        internal static extern IntPtr BIO_s_socket();
-
-        /// <summary>
-        /// Creates new <c>BIO</c> object in unmanaged memory.
-        /// <see href="https://docs.openssl.org/3.4/man3/BIO_new/"/>
-        /// </summary>
-        /// <param name="method">Fetch using <see cref="BIO_s_socket"/></param>
-        /// <returns>Pointer to new <c>BIO</c> object or <c>IntPtr.Zero</c> on error.</returns>
-        /// <remarks>Return of IntPtr.Zero is untested!</remarks>
-        [DllImport("libcrypto-3.dll", CallingConvention = CallingConvention.Cdecl)]
-        internal static extern IntPtr BIO_new(IntPtr method);
-
-        /// <summary>
-        /// BIO control operation using integer parameters.
-        /// <see href="https://docs.openssl.org/3.4/man3/BIO_ctrl/"/>
-        /// </summary>
-        /// <param name="bio">Pointer to <c>BIO</c> object</param>
-        /// <param name="cmd">One of the <c>BIO_C_*</c> <see cref="Constants"/></param>
-        /// <param name="larg">First argument</param>
-        /// <param name="iarg">Second argument</param>
-        /// <returns>Varies based on cmd</returns>
-        [DllImport("libcrypto-3.dll", CallingConvention = CallingConvention.Cdecl)]
-        internal static extern int BIO_int_ctrl(IntPtr bio, int cmd, int larg, int iarg);
-
-        /// <summary>
-        /// Attaches file descriptor to BIO.
-        /// </summary>
-        /// <param name="bio">Pointer to BIO object</param>
-        /// <param name="fd">File descriptor number, typically socket number</param>
-        /// <param name="flags">Either of <c>BIO_NOCLOSE</c> or <c>BIO_CLOSE</c> <see cref="Constants"/></param>
-        /// <returns></returns>
-        internal static int BIO_set_fd(IntPtr bio, int fd, int flags)
-        {
-            return BIO_int_ctrl(bio, Constants.BIO_C_SET_FD, flags, fd);
-        }
-    }
-
-    /// <summary>
     /// Manages representation of BIO (basic I/O) c objects.
     /// </summary>
     /// <remarks>
@@ -157,7 +14,7 @@ namespace OpenSSLWebClient.Components
     {
         private protected IntPtr _bio;
         private protected int _socket;
-        private bool disposed = false;
+        private bool _disposed = false;
 
         public readonly string hostname;
         public readonly string port;
@@ -319,7 +176,7 @@ namespace OpenSSLWebClient.Components
         /// <param name="disposing">True when disposing, false during finalization.</param>
         protected virtual void Dispose(bool disposing)
         {
-            if (!disposed)
+            if (!_disposed)
             {
                 if (disposing)
                 {
@@ -329,7 +186,7 @@ namespace OpenSSLWebClient.Components
 
                 _bio = IntPtr.Zero;
                 _socket = -1;
-                disposed = true;
+                _disposed = true;
             }
         }
 
---
+++ OpenSSLWebClient/Components/SSLInterop.cs
@@ -0,0 +1,300 @@
+﻿using System;
+using System.Runtime.InteropServices;
+
+namespace OpenSSLWebClient.Components
+{
+    /// <summary>
+    /// Stores P/Invoke definitions for managing SSL objects
+    /// </summary>
+    internal static class SSLInterop
+    {
+        /// <summary>
+        /// <see href="https://docs.openssl.org/3.4/man3/SSL_CTX_new/"/>
+        /// </summary>
+        /// <returns>Pointer to the TLS client method</returns>
+        [DllImport("libssl-3.dll", CallingConvention = CallingConvention.Cdecl)]
+        public static extern IntPtr TLS_client_method();
+
+        /// <summary>
+        /// <see href="https://docs.openssl.org/3.4/man3/SSL_CTX_new/"/>
+        /// </summary>
+        /// <returns>Pointer to the DTLS client method</returns>
+        [DllImport("libssl-3.dll", CallingConvention = CallingConvention.Cdecl)]
+        public static extern IntPtr DTLS_client_method();
+
+        /// <summary>
+        /// <see href="https://docs.openssl.org/3.4/man3/OSSL_QUIC_client_method/"/>
+        /// </summary>
+        /// <returns>Pointer to the QUIC client method</returns>
+        [DllImport("libssl-3.dll", CallingConvention = CallingConvention.Cdecl)]
+        public static extern IntPtr OSSL_QUIC_client_method();
+
+        /// <summary>
+        /// <see href="https://docs.openssl.org/3.4/man3/OSSL_QUIC_client_method/"/>
+        /// </summary>
+        /// <returns>Pointer to the QUIC client thread method</returns>
+        [DllImport("libssl-3.dll", CallingConvention = CallingConvention.Cdecl)]
+        public static extern IntPtr OSSL_QUIC_client_thread_method();
+
+        /// <summary>
+        /// Create new SSL CTX object.
+        /// <see href="https://docs.openssl.org/3.4/man3/SSL_CTX_new/"/>
+        /// </summary>
+        /// <param name="method">Pointer to SSL method (<see cref="TLS_client_method"/>)</param>
+        /// <returns>Pointer to SSL CTX object in unmanaged memory</returns>
+        [DllImport("libssl-3.dll", CallingConvention = CallingConvention.Cdecl)]
+        public static extern IntPtr SSL_CTX_new(IntPtr method);
+
+        /// <summary>
+        /// Set verification level for SSL objects created by this context.
+        /// <see href="https://docs.openssl.org/3.4/man3/SSL_CTX_set_verify/"/>
+        /// </summary>
+        /// <param name="ctx">Pointer to SSL CTX object</param>
+        /// <param name="verify">
+        /// Verification level from <c>SSL_VERIFY_*</c> <see cref="Constants"/>
+        /// (see <see href="https://docs.openssl.org/3.4/man3/SSL_CTX_set_verify/#notes"/> for more details)
+        /// </param>
+        /// <param name="cb">Callback function to be used, can be <c>IntPtr.Zero</c> for no callback</param>
+        [DllImport("libssl-3.dll", CallingConvention = CallingConvention.Cdecl)]
+        public static extern void SSL_CTX_set_verify(IntPtr ctx, int verify, IntPtr cb);
+
+        /// <summary>
+        /// Sets SSL CTX object to use default locations for CA certificates.
+        /// <see href="https://docs.openssl.org/3.4/man3/SSL_CTX_load_verify_locations/"/>
+        /// </summary>
+        /// <param name="ctx">Pointer to SSL CTX object</param>
+        /// <returns>1 on successful operation, 0 on error</returns>
+        [DllImport("libssl-3.dll", CallingConvention = CallingConvention.Cdecl)]
+        public static extern int SSL_CTX_set_default_verify_paths(IntPtr ctx);
+
+        /// <summary>
+        /// Configures minumum protocol version that can be used by a connection.
+        /// <see href="https://docs.openssl.org/3.4/man3/SSL_CTX_set_min_proto_version/"/>
+        /// </summary>
+        /// <param name="ctx">Pointer to CTX object</param>
+        /// <param name="version">Supported <see cref="Constants"/> are <c>TLS1_2_VERSION</c> and <c>TLS1_3_VERSION</c></param>
+        /// <returns>1 on success, 0 on failure</returns>
+        public static int SSL_CTX_set_min_proto_version(IntPtr ctx, int version)
+        {
+            return SSL_CTX_ctrl(ctx, Constants.SSL_CTRL_SET_MIN_PROTO_VERSION, version, IntPtr.Zero);
+        }
+
+        /// <summary>
+        /// Configures maximum protocol version that can be used by a connection.
+        /// <see href="https://docs.openssl.org/3.4/man3/SSL_CTX_set_min_proto_version/"/>
+        /// </summary>
+        /// <param name="ctx">Pointer to CTX object</param>
+        /// <param name="version">Supported <see cref="Constants"/> are <c>TLS1_2_VERSION</c> and <c>TLS1_3_VERSION</c></param>
+        /// <returns>1 on success, 0 on failure</returns>
+        public static int SSL_CTX_set_max_proto_version(IntPtr ctx, int version)
+        {
+            return SSL_CTX_ctrl(ctx, Constants.SSL_CTRL_SET_MAX_PROTO_VERSION, version, IntPtr.Zero);
+        }
+
+        /// <summary>
+        /// Gets minumum protocol version that can be used by a connection.
+        /// <see href="https://docs.openssl.org/3.4/man3/SSL_CTX_set_min_proto_version/"/>
+        /// </summary>
+        /// <param name="ctx">Pointer to CTX object</param>
+        /// <returns>Integer equivalent to TLS1_?_VERSION <see cref="Constants"/></returns>
+        public static int SSL_CTX_get_min_proto_version(IntPtr ctx)
+        {
+            return SSL_CTX_ctrl(ctx, Constants.SSL_CTRL_GET_MIN_PROTO_VERSION, 0, IntPtr.Zero);
+        }
+
+        /// <summary>
+        /// Gets maximum protocol version that can be used by a connection.
+        /// <see href="https://docs.openssl.org/3.4/man3/SSL_CTX_set_min_proto_version/"/>
+        /// </summary>
+        /// <param name="ctx">Pointer to CTX object</param>
+        /// <returns>Integer equivalent to TLS1_?_VERSION <see cref="Constants"/></returns>
+        public static int SSL_CTX_get_max_proto_version(IntPtr ctx)
+        {
+            return SSL_CTX_ctrl(ctx, Constants.SSL_CTRL_GET_MAX_PROTO_VERSION, 0, IntPtr.Zero);
+        }
+
+        /// <summary>
+        /// Performs manipulation of SSL CTX objects. Behaves differently depending on supplied cmd.
+        /// <see href="https://docs.openssl.org/3.4/man3/SSL_CTX_ctrl/"/>
+        /// </summary>
+        /// <param name="ctx">Pointer to SSL CTX object</param>
+        /// <param name="cmd">One of the <c>SSL_CTRL_*</c> <see cref="Constants"/></param>
+        /// <param name="larg">Integer argument</param>
+        /// <param name="parg">Pointer argument</param>
+        [DllImport("libssl-3.dll", CallingConvention = CallingConvention.Cdecl)]
+        public static extern int SSL_CTX_ctrl(IntPtr ctx, int cmd, int larg, IntPtr parg);
+
+        /// <summary>
+        /// Configures the CAfile and CApath used for validating certificates.
+        /// For additional information on CAfile and CApath, review the linked openssl documentation:
+        /// <see href="https://docs.openssl.org/3.4/man3/SSL_CTX_load_verify_locations"/>
+        /// </summary>
+        /// <remarks>
+        /// Failure occurs when CAfile and CApath are both null, and additionally when processing one of those locations fails.
+        /// </remarks>
+        /// <param name="ctx">Pointer to SSL CTX object</param>
+        /// <param name="CAfile">Path to a PEM encoded file containing one or more certificates</param>
+        /// <param name="CApath">Path to a directory containing one or more individually packaged certificates</param>
+        /// <returns>
+        /// 1 on successful operation, 0 on failure.
+        /// </returns>
+        [DllImport("libssl-3.dll", CallingConvention = CallingConvention.Cdecl)]
+        public static extern int SSL_CTX_load_verify_locations(IntPtr ctx, string CAfile, string CApath);
+
+        /// <summary>
+        /// Creates new SSL object from supplied context
+        /// <see href="https://docs.openssl.org/3.4/man3/SSL_new/"/>
+        /// </summary>
+        /// <param name="ctx">Pointer to SSL CTX object</param>
+        /// <returns>Pointer to SSL object or <c>IntPtr.Zero</c> on failure</returns>
+        /// <remarks>Return of <c>IntPtr.Zero</c> is untested!</remarks>
+        [DllImport("libssl-3.dll", CallingConvention = CallingConvention.Cdecl)]
+        public static extern IntPtr SSL_new(IntPtr ctx);
+
+        /// <summary>
+        /// Configures the BIO(s) to be used by this SSL object.
+        /// <see href="https://docs.openssl.org/3.4/man3/SSL_set_bio/"/>
+        /// </summary>
+        /// <remarks>
+        /// <c>read_bio</c> and <c>write_bio</c> can (and likely often should) be the same.
+        /// </remarks>
+        /// <param name="ssl">Pointer to SSL object</param>
+        /// <param name="read_bio">Pointer to BIO object used for reads</param>
+        /// <param name="write_bio">Pointer to BIO object used for writes</param>
+        [DllImport("libssl-3.dll", CallingConvention = CallingConvention.Cdecl)]
+        public static extern void SSL_set_bio(IntPtr ssl, IntPtr read_bio, IntPtr write_bio);
+
+        /// <summary>
+        /// Congigures the server name indication ClientHello extension to contain the specified name.
+        /// <see href="https://docs.openssl.org/3.4/man3/SSL_CTX_set_tlsext_servername_callback/"/>
+        /// </summary>
+        /// <remarks>
+        /// If improperly configured, the server may reject the handshake.
+        /// </remarks>
+        /// <param name="ssl">Pointer to SSL object</param>
+        /// <param name="name">Hostname of remote server</param>
+        /// <returns></returns>
+        public static int SSL_set_tlsext_host_name(IntPtr ssl, string name)
+        {
+            return SSL_ctrl(ssl, Constants.SSL_CTRL_SET_TLSEXT_HOSTNAME, Constants.TLSEXT_NAMETYPE_host_name, name); ;
+        }
+
+        /// <summary>
+        /// Performs manipulation of SSL objects. Behaves differently depending on supplied cmd.
+        /// <see href="https://docs.openssl.org/3.4/man3/SSL_CTX_ctrl/"/>
+        /// </summary>
+        /// <param name="ctx">Pointer to SSL object</param>
+        /// <param name="cmd">One of the <c>SSL_CTRL_*</c> <see cref="Constants"/></param>
+        /// <param name="larg">Integer argument</param>
+        /// <param name="parg">Pointer argument</param>
+        [DllImport("libssl-3.dll", CallingConvention = CallingConvention.Cdecl)]
+        public static extern int SSL_ctrl(IntPtr ssl, int cmd, int larg, string parg);
+
+        /// <summary>
+        /// Initiate the TLS/SSL handshake with the remote server.
+        /// <see href="https://docs.openssl.org/3.4/man3/SSL_connect/"/>
+        /// </summary>
+        /// <param name="ssl">Pointer to SSL object</param>
+        /// <returns>1 on success, 0 on error</returns>
+        [DllImport("libssl-3.dll", CallingConvention = CallingConvention.Cdecl)]
+        public static extern int SSL_connect(IntPtr ssl);
+
+        /// <summary>
+        /// Obtain the result code for a TLS/SSL operation.
+        /// <see href="https://docs.openssl.org/3.4/man3/SSL_get_error/"/>
+        /// </summary>
+        /// <remarks>
+        /// Must be called immediately after failure to obtain results.
+        /// </remarks>
+        /// <param name="ssl">Pointer to SSL object</param>
+        /// <param name="ret">Value returned by the previous TLS/SSL function</param>
+        /// <returns></returns>
+        [DllImport("libssl-3.dll", CallingConvention = CallingConvention.Cdecl)]
+        public static extern int SSL_get_error(IntPtr ssl, int ret);
+
+        [DllImport("libssl-3.dll", CallingConvention = CallingConvention.Cdecl)]
+        public static extern int SSL_get_verify_result(IntPtr ssl);
+
+        /// <summary>
+        /// Closes SSL connection with remote server.
+        /// <see href="https://docs.openssl.org/3.4/man3/SSL_shutdown/"/>
+        /// </summary>
+        /// <remarks>
+        /// Failure can occur whe not all data has been read
+        /// </remarks>
+        /// <param name="ssl">Pointer to SSL object</param>
+        /// <returns>0 for ongoing but incomplete shutdown, 1 for successful, less than 0 for not successful</returns>
+        [DllImport("libssl-3.dll", CallingConvention = CallingConvention.Cdecl)]
+        public static extern int SSL_shutdown(IntPtr ssl);
+
+        /// <summary>
+        /// Frees allocated SSL structure, including other items, such as the read and write BIOs.
+        /// <see href="https://docs.openssl.org/3.4/man3/SSL_free/"/>
+        /// </summary>
+        /// <param name="ssl">Pointer to SSL object</param>
+        [DllImport("libssl-3.dll", CallingConvention = CallingConvention.Cdecl)]
+        public static extern void SSL_free(IntPtr ssl);
+
+        /// <summary>
+        /// Frees allocated SSL context structure.
+        /// <see href="https://docs.openssl.org/3.4/man3/SSL_CTX_free/"/>
+        /// </summary>
+        /// <param name="ssl">Pointer to SSL CTX object</param>
+        [DllImport("libssl-3.dll", CallingConvention = CallingConvention.Cdecl)]
+        public static extern void SSL_CTX_free(IntPtr ctx);
+
+        /// <summary>
+        /// Writes data over the SSL connection.
+        /// <see href="https://docs.openssl.org/3.4/man3/SSL_write/"/>
+        /// </summary>
+        /// <param name="ssl">Pointer to SSL object</param>
+        /// <param name="buf">Contents to be sent to the peer</param>
+        /// <param name="num">Number of bytes to send to the peer</param>
+        /// <param name="written">Pointer to an integer that will contain the number of bytes written to the peer</param>
+        /// <returns>1 on success, 0 on failure</returns>
+        [DllImport("libssl-3.dll", CallingConvention = CallingConvention.Cdecl)]
+        public static extern int SSL_write_ex(IntPtr ssl, string buf, UIntPtr num, IntPtr written);
+
+        /// <summary>
+        /// Reads data over the SSL connection.
+        /// <see href="https://docs.openssl.org/3.4/man3/SSL_read/"/>
+        /// </summary>
+        /// <param name="ssl">Pointer to SSL object</param>
+        /// <param name="buf">Pointer to start of buffer</param>
+        /// <param name="num">Maxium bytes to read from the peer</param>
+        /// <param name="written">Pointer to an integer that will contain the number of bytes read from the peer</param>
+        /// <returns>1 on success, 0 on failure</returns>
+        [DllImport("libssl-3.dll", CallingConvention = CallingConvention.Cdecl)]
+        public static extern int SSL_read_ex(IntPtr ssl, IntPtr buf, UIntPtr num, IntPtr readbytes);
+
+        /// <summary>
+        /// Configures the hostname that the peer's certificate must contain in order for the certificate to be validated.
+        /// <see href="https://docs.openssl.org/3.4/man3/SSL_set1_host/"/>
+        /// </summary>
+        /// <param name="ssl">Pointer to SSL object</param>
+        /// <param name="hostname">Expected hostname of server</param>
+        /// <returns></returns>
+        [DllImport("libssl-3.dll", CallingConvention = CallingConvention.Cdecl)]
+        public static extern int SSL_set1_host(IntPtr ssl, string hostname);
+
+        /// <summary>
+        /// Received TCP records that have been processed and ready to be read are stored in a buffer.
+        /// Calling this method before <c>SSL_read</c> will help prevent accidentally blocking on a read with no data.
+        /// See also: <see cref="https://docs.openssl.org/3.4/man3/SSL_pending/"/>
+        /// </summary>
+        /// <param name="ssl">Pointer to SSL object</param>
+        /// <returns>Number of bytes buffered and ready for <c>SSL_read</c></returns>
+        [DllImport("libssl-3.dll", CallingConvention = CallingConvention.Cdecl)]
+        public static extern int SSL_pending(IntPtr ssl);
+
+        /// <summary>
+        /// Received TCP records that have been processed and ready to be read are stored in a buffer.
+        /// Calling this method before <c>SSL_read</c> will help prevent accidentally blocking on a read with no data.
+        /// See also: <see cref="https://docs.openssl.org/3.4/man3/SSL_pending/"/>
+        /// </summary>
+        /// <param name="ssl">Pointer to SSL object</param>
+        /// <returns>1 if SSL has pending records ready to read, 0 if not</returns>
+        [DllImport("libssl-3.dll", CallingConvention = CallingConvention.Cdecl)]
+        public static extern int SSL_has_pending(IntPtr ssl);
+    }
+}
